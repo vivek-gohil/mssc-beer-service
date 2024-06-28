e26a67cf3db4ea41567b8d34535538c487d114c0
Vivek Gohil <ghl_vivek@hotmail.com>
2024-06-28 13:37:37 +0530

    Initial Commit

---
+++ src/main/java/com/borntocode/main/MsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.borntocode.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceApplication.class, args);
+	}
+
+}
